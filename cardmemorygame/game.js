af753ba5376833ffe8646391cda11c17795d3f54
Camile Marcele <mcamileparaujo@gmail.com>
2023-11-12 23:19:54 -0300

    update

--- cardmemorygame/game.js
+++ cardmemorygame/game.js
@@ -1,13 +1,26 @@
-const cards =
+const flipContainer =
     document.getElementsByClassName("flip-container")
-for (let i=0; i<cards.length; i++){
-    cards[i].addEventListener("click",addClassList)
+for (let i=0; i<flipContainer.length; i++){
+    flipContainer[i].addEventListener("click",addClassList)
      
 }
 function addClassList(event){
-    const card = event.currentTarget
-    const id=card.getAttribute("id")
-    card.classList.toggle("flip-container-flipped")
+    const flipContainer = event.currentTarget
+
+    const id=flipContainer.getAttribute("id")
+
+    flipContainer.classList.toggle("flip-container-flipped")
     console.log( document.getElementById(id+"-img"))
 }
+// const cards = document.querySelectorAll('.flipper')
+// let hasFlippedCard= false;
+// let firstCard,secondCard;
 
+// function flipCard(){
+//     this.classList.add('flip');
+//     if(!hasFlippedCard){
+//         hasFlippedCard=true;
+//         firstCard=this;
+//     }
+// }
+// cards.forEach(card=>card.addEventListener('click',flipCard));
